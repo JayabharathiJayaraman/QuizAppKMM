3054311f296f1abbc36cdce158f3ea6d1658ea9e
JayabharathiJayaraman <jayabharathi.jayaraman@iths.se>
2022-05-17 11:58:18 +0200

    initial commit

---
+++ QuizAppKMM/androidApp/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    compileSdk = 32
+    defaultConfig {
+        applicationId = "com.example.quizapp.android"
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.2")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+}
